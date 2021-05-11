b4c41c3f888cc893b43dfe08efaeefdf3a8f12b2
Austin <sicardaustin97@gmail.com>
2021-05-11 14:32:18 -0400

    W7D1 Assignments complete commiting to keep organized

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/DayListItem.scss"
+import classNames from "classnames/bind"
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.spots === 0
+  });
+  const formatSpots = () => {
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    } else if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
+    } else {
+      return "no spots remaining";
+    }
+  }
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
