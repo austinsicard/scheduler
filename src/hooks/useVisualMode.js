a5a8cd0c70667c1bcd858407cfb21848ad0ef73f
Austin <sicardaustin97@gmail.com>
2021-06-25 14:37:49 -0400

    Fixed useVisualMode.js to update current mode by saving it to history and giving the option of reverting to the previous mode. Did this by adding a function that accepts newMode and defaults replace to false. Included a back function that controls going to the previous mode and not allowing to go back further by including history.length = 1

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
- export default function useVisualMode(initial) { 
+// Custom Hook to change between components
+function useVisualMode(initial) {
 
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+
+  // Updates the current mode and saves it to history, can revert to previous mode if specified
   function transition(newMode, replace = false) {
-    setMode(newMode)
     if (replace) {
-      setHistory(prev => [prev[0]])
+      setHistory((prev) => [...prev.slice(0, -1), newMode])
+    } else {
+      setHistory((prev) => [...prev, newMode]);
     }
-    
-    setHistory(prev => [...prev, newMode])
+  }
+  // Back function goes to previous mode
+  function back() {
+    // Prevent reverting more than once
+    if (history.length <= 1) return;
+    setHistory((prev) => [...prev.slice(0, -1)]);
   }
 
-   function back() {
-    //  console.log(history)
-     history.pop();
-     if (history.length) {
-       return setMode(history[history.length - 1 ]);
-     }
-   }
-
-  return { mode, transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
+
+export default useVisualMode;
